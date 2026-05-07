d518d20d1891ce50e7c371feea71bdc490fb8635
revathisaravanan2006 <rkcraftworld@gmail.com>
2026-05-07 07:15:06 +0530

    Delete exam_backend/demo directory

--- exam_backend/demo/src/main/java/com/example/demo/repositories/DebateRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.entities.Debate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DebateRepo extends JpaRepository<Debate, Integer> {
-}
-
--- exam_backend/demo/src/main/java/com/example/demo/repositories/ParticipationRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.entities.Participation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParticipationRepo extends JpaRepository<Participation, Integer> {
-}
-
